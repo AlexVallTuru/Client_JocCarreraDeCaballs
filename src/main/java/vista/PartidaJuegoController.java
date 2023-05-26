de257998972a5a255fb169716e368328f71680c6
Aitor Pérez Romero <elterrasense@gmail.com>
2023-05-26 10:14:24 +0200

    Commit de emergencia, implementación popups
    Implementación inicial de popups de victoria y derrota y algunas mejoras a la interfaz, commit para guardar progreso ya que la maquina está dando problemas.

--- src/main/java/vista/PartidaJuegoController.java
+++ src/main/java/vista/PartidaJuegoController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -27,7 +28,7 @@ import static vista.MainController.partida;
 public class PartidaJuegoController implements Initializable {
 
     int idPartida;
-    
+
     @FXML
     private ImageView imagePalo;
 
@@ -44,6 +45,7 @@ public class PartidaJuegoController implements Initializable {
     private ObjetoPartida puntuacionNueva;
     private String palo;
     private int dificultad;
+    private int cartaCount = 0;
 
     private String email;
     private String nickname;
@@ -64,20 +66,34 @@ public class PartidaJuegoController implements Initializable {
 
     @FXML
     private void pedirCarta() throws PartidaException, IOException {
-        // Lógica para obtener una nueva carta y actualizar la puntuación
-        // Por ahora, simplemente incrementaremos la puntuación actual en 1
+        cartaCount++;
+        // Llamada a la lógica del juego para obtener una nueva carta
         puntuacionNueva = partida.partidaLogica(puntuacionActual, palo, dificultad);
         if (puntuacionNueva.isIsFinished()) {
+            // Si nos hemos quedado sin cartas, termina la partida y no se guarda una puntuación
             puntuacionActual = 0;
-        } else if (puntuacionNueva != null){
+            mostrarResultadoPartida("Perdiste la partida.", "Te has "
+                    + "quedado sin cartas.");
+        } else if (puntuacionNueva != null) {
+            // Si obtenemos una nueva carta, sumamos la puntuación y la mostramos
             puntuacionActual = puntuacionNueva.getScore();
             Image cartaImage = new Image(getClass().getResourceAsStream("cartas/" + puntuacionNueva.getImageName()));
             cartaImageView.setImage(cartaImage);
         }
-        
-        puntuacionLabel.setText("Puntuación actual: " + puntuacionActual);
-        
+
+        puntuacionLabel.setText("Puntuación actual: " + puntuacionActual + "/38");
+
         if (puntuacionActual >= 38 || puntuacionNueva.isIsFinished()) {
+            if (puntuacionActual == 38) {
+                mostrarResultadoPartida("¡Ganaste la partida!", "¡Has"
+                        + " obtenido una puntuación perfecta en " + cartaCount
+                        + " cartas!");
+            } else {
+                mostrarResultadoPartida("¡Ganaste la partida!", "¡Has "
+                        + "llegado a " + puntuacionActual + " de 38 puntos, "
+                        + (puntuacionActual - 38) + " por encima de la "
+                        + "puntuación perfecta en " + cartaCount + " cartas!");
+            }
 
             System.out.print("\nIDPARTIDA: " + idPartida + " PUNTUACIONACTUAL: " + puntuacionActual + "\n");
 
@@ -96,20 +112,28 @@ public class PartidaJuegoController implements Initializable {
             // Obtener el controlador de la escena principal
             MainController mainController = loader.getController();
             // Pasar los valores a través del método set en el controlador de la escena principal
-            mainController.setEmailAndNickname(email,nickname);
+            mainController.setEmailAndNickname(email, nickname);
 
             // Obtener la ventana actual y cerrarla
             Stage currentStage = (Stage) pedirCartaButton.getScene().getWindow();
             currentStage.close();
         }
     }
-    
+
+    private void mostrarResultadoPartida(String header, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Fin de la partida");
+        alert.setHeaderText(header);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
     public void setPalo(String paloIn) {
         this.palo = paloIn;
         Image cartaImage = new Image(getClass().getResourceAsStream("cartas/" + palo + ".png"));
         imagePalo.setImage(cartaImage);
     }
-    
+
     public void setDificultad(int dificultad) {
         this.dificultad = dificultad;
     }
